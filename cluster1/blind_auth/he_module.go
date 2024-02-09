0b3bfa2bb01e428323e3d5886323561975dffa16
hyunmin.choi <hyunmin.choi@navercorp.com>
2024-02-09 16:11:26 +0900

    Add cluster1

---
+++ cluster1/blind_auth/he_module.go
@@ -0,0 +1,147 @@
+package blind_auth
+
+import (
+	"github.com/tuneinsight/lattigo/v5/core/rlwe"
+	"github.com/tuneinsight/lattigo/v5/he/hefloat"
+)
+
+type KeySet struct {
+	Pk  *rlwe.PublicKey
+	Sk  *rlwe.SecretKey
+	Rlk *rlwe.RelinearizationKey
+	Gks []*rlwe.GaloisKey
+}
+
+type ObjectSet struct {
+	Ecd  *hefloat.Encoder
+	Enc  *rlwe.Encryptor
+	Dec  *rlwe.Decryptor
+	Eval *hefloat.Evaluator
+}
+
+func GenKeyPair(params hefloat.Parameters) *KeySet {
+	galEls := []uint64{params.GaloisElement(-32), params.GaloisElement(-64), params.GaloisElement(-128)}
+	for i := 0; i < 9; i++ {
+		galEls = append(galEls, params.GaloisElement(PowerOfTwo(i)))
+	}
+	for i := 0; i < 17; i++ {
+		galEls = append(galEls, params.GaloisElement((-1)*i))
+	}
+	kgen := rlwe.NewKeyGenerator(params)
+	sk := kgen.GenSecretKeyNew()
+	pk := kgen.GenPublicKeyNew(sk)
+	rlk := kgen.GenRelinearizationKeyNew(sk)
+	gks := kgen.GenGaloisKeysNew(galEls, sk)
+	keySet := &KeySet{}
+	keySet.Pk, keySet.Sk, keySet.Rlk, keySet.Gks = pk, sk, rlk, gks
+
+	return keySet
+}
+
+func GetKeyPair(params hefloat.Parameters, path string, logN int) *ObjectSet {
+	pk := &rlwe.PublicKey{}
+	sk := &rlwe.SecretKey{}
+	rlk := &rlwe.RelinearizationKey{}
+
+	pk_b, err := os.ReadFile(path + "/public_key/0")
+	ReturnErr(err)
+	ReturnErr(pk.UnmarshalBinary(pk_b))
+
+	sk_b, err := os.ReadFile(path + "/secret_key/0")
+	ReturnErr(err)
+	ReturnErr(sk.UnmarshalBinary(sk_b))
+
+	relin_b, err := os.ReadFile(path + "/relin_key/0")
+	ReturnErr(err)
+	ReturnErr(rlk.UnmarshalBinary(relin_b))
+
+	gks := []*rlwe.GaloisKey{}
+	for i := 0; i < 29; i++ {
+		galkey := &rlwe.GaloisKey{}
+		gk_b, err := os.ReadFile(path + "/galois_key/" + strconv.Itoa(i))
+		ReturnErr(err)
+		ReturnErr(galkey.UnmarshalBinary(gk_b))
+		gks = append(gks, galkey)
+	}
+	evk := rlwe.NewMemEvaluationKeySet(rlk, gks...)
+
+	obs := &ObjectSet{}
+	ecd := hefloat.NewEncoder(params)
+	enc := hefloat.NewEncryptor(params, pk)
+	dec := hefloat.NewDecryptor(params, sk)
+	eval := hefloat.NewEvaluator(params, evk)
+
+	obs.Ecd, obs.Enc, obs.Dec, obs.Eval = ecd, enc, dec, eval
+	return obs
+}
+
+func HE_Cossim(params hefloat.Parameters, o *ObjectSet, input_ct_b []byte, NUM_CTXT int, LOG_NUM_CTXT int, LOG_FV_SIZE int, masks [][]float64, ctxt_dbs [][]*rlwe.Ciphertext) []byte {
+	NUM_INPUT_CTXT := PowerOfTwo(LOG_NUM_CTXT)
+	FV_SIZE := PowerOfTwo(LOG_FV_SIZE)
+	VEC_SIZE := FV_SIZE / NUM_INPUT_CTXT
+	test := make([]float64, NUM_CTXT)
+	for i := 0; i < len(test); i++ {
+		if i%VEC_SIZE == 0 {
+			test[i] = 1.0
+		}
+	}
+	// mask := hefloat.NewPlaintext(params, 1)
+	// o.Ecd.Encode(test, mask)
+	input_ctxt := &rlwe.Ciphertext{}
+	input_ctxt.UnmarshalBinary(input_ct_b)
+
+	input_ctxts := make([]*rlwe.Ciphertext, NUM_INPUT_CTXT)
+	for i := 0; i < NUM_INPUT_CTXT; i++ {
+		c, err := o.Eval.MulRelinNew(input_ctxt, masks[i])
+		o.Eval.Rescale(c, c)
+		ReturnErr(err)
+		for j := 0; j < LOG_NUM_CTXT; j++ {
+			tmp, _ := o.Eval.RotateNew(c, Tmp_func(i, PowerOfTwo(j))*PowerOfTwo(j+LOG_FV_SIZE-LOG_NUM_CTXT))
+			o.Eval.Add(c, tmp, c)
+		}
+		input_ctxts[i] = c
+	}
+
+	result := &rlwe.Ciphertext{}
+	for i := 0; i < len(ctxt_dbs); i++ {
+		if len(ctxt_dbs[0]) == 0 {
+			fmt.Println("")
+		} else {
+			c, err := o.Eval.MulRelinNew(input_ctxts[0], ctxt_dbs[i][0])
+			ReturnErr(err)
+			o.Eval.Rescale(c, c)
+
+			for j := 1; j < len(ctxt_dbs[0]); j++ {
+				tmp, err := o.Eval.MulRelinNew(input_ctxts[j], ctxt_dbs[i][j])
+				o.Eval.Rescale(tmp, tmp)
+				ReturnErr(err)
+				o.Eval.Add(c, tmp, c)
+			}
+			//////////////////////////////////////////////////////////
+			// For Compression methods
+			//////////////////////////////////////////////////////////
+			for k := 0; k < LOG_FV_SIZE-LOG_NUM_CTXT; k++ {
+				tmp, err := o.Eval.RotateNew(c, PowerOfTwo(k))
+				ReturnErr(err)
+				o.Eval.Add(c, tmp, c)
+			}
+			o.Eval.MulRelin(c, test, c)
+			if i == 0 {
+				result = c
+			} else {
+				o.Eval.Rotate(c, (-1)*i, c)
+				o.Eval.Add(result, c, result)
+			}
+		}
+	}
+	result_b, err := result.MarshalBinary()
+	ReturnErr(err)
+	return result_b
+}
+
+func SetTestParams(KEY_PATH string, LogN int, FV_SIZE int, NUM_CTXT int, VEC_SIZE int) [][]float64 {
+	params := Blind_Auth_Param(LogN)
+	o := GetKeyPair(params, KEY_PATH, LogN)
+	masks := GenMaskVector(params, o, FV_SIZE, VEC_SIZE)
+	return masks
+}
---
+++ cluster1/main.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	api "github.com/hm-choi/blind_api"
+	"github.com/hm-choi/blind_auth"
+)
+
+type Response struct {
+	Message    string `json:"message"`
+	Data       string `json:"data"`
+	clusterIdx int
+}
+
+func main() {
+	router := gin.Default()
+	router.Use(api.CORSMiddleware())
+
+	CLUSTER_IDX := 0
+
+	// LogN means the degree parameter of CKKS. In our setting, LogN = 13
+	LogN := 13
+	NUM_CTXT := blind_auth.PowerOfTwo(LogN)
+
+	// KEY_PATH is the set of path of the stored key set.
+	// CTXT_PATH_128 is the path that the saved ciphertext with feature vector size 128 in the server
+	// CTXT_PATH_64 is the path that the saved ciphertext with feature vector size 64 in the server
+	KEY_PATH := "/root/src/blind_auth_new/key/depth3"
+	CTXT_PATH_128 := "/root/src/blind_auth_new/ctxt/cluster1/r18_feat128_ms1mv3/"
+	CTXT_PATH_64 := "/root/src/blind_auth_new/ctxt/cluster1/r18_feat64_ms1mv3/"
+
+	params := blind_auth.Blind_Auth_Param(LogN)
+	o := blind_auth.GetKeyPair(params, KEY_PATH, LogN)
+
+	ctxt_dbs_128_0 := blind_auth.GetCtxtDbs(CTXT_PATH_128, 32, 0, 1)
+	ctxt_dbs_128_1 := blind_auth.GetCtxtDbs(CTXT_PATH_128, 16, 1, 2)
+	ctxt_dbs_128_2 := blind_auth.GetCtxtDbs(CTXT_PATH_128, 8, 2, 4)
+	ctxt_dbs_128_3 := blind_auth.GetCtxtDbs(CTXT_PATH_128, 4, 3, 8)
+	ctxt_dbs_128_4 := blind_auth.GetCtxtDbs(CTXT_PATH_128, 2, 4, 16)
+	ctxt_dbs_128_5 := blind_auth.GetCtxtDbs(CTXT_PATH_128, 1, 5, 32)
+
+	masks_64_0 := blind_auth.SetTestParams(KEY_PATH, LogN, 64, 1, 64)
+	masks_64_1 := blind_auth.SetTestParams(KEY_PATH, LogN, 64, 2, 32)
+	masks_64_2 := blind_auth.SetTestParams(KEY_PATH, LogN, 64, 4, 16)
+	masks_64_3 := blind_auth.SetTestParams(KEY_PATH, LogN, 64, 8, 8)
+	masks_64_4 := blind_auth.SetTestParams(KEY_PATH, LogN, 64, 16, 4)
+
+	ctxt_dbs_64_0 := blind_auth.GetCtxtDbs(CTXT_PATH_64, 16, 0, 1)
+	ctxt_dbs_64_1 := blind_auth.GetCtxtDbs(CTXT_PATH_64, 8, 1, 2)
+	ctxt_dbs_64_2 := blind_auth.GetCtxtDbs(CTXT_PATH_64, 4, 2, 4)
+	ctxt_dbs_64_3 := blind_auth.GetCtxtDbs(CTXT_PATH_64, 2, 3, 8)
+	ctxt_dbs_64_4 := blind_auth.GetCtxtDbs(CTXT_PATH_64, 1, 4, 16)
+
+	masks_128_0 := blind_auth.SetTestParams(KEY_PATH, LogN, 128, 1, 128)
+	masks_128_1 := blind_auth.SetTestParams(KEY_PATH, LogN, 128, 2, 64)
+	masks_128_2 := blind_auth.SetTestParams(KEY_PATH, LogN, 128, 4, 32)
+	masks_128_3 := blind_auth.SetTestParams(KEY_PATH, LogN, 128, 8, 16)
+	masks_128_4 := blind_auth.SetTestParams(KEY_PATH, LogN, 128, 16, 8)
+	masks_128_5 := blind_auth.SetTestParams(KEY_PATH, LogN, 128, 32, 4)
+
+	// ======================================================== //
+	// Define API
+	// ======================================================== //
+
+	// ======================================================== //
+	// Api for feature vector size = 128
+	// ======================================================== //
+	router.POST("/send-ctxt/face/128/13-0", api.CORSMiddleware(), func(c *gin.Context) {
+		START_TIME := time.Now()
+		var body api.RequestBody
+		err := json.NewDecoder(c.Request.Body).Decode(&body)
+		blind_auth.ReturnErr(err)
+		result_b := blind_auth.HE_Cossim(params, o, body.Ctxt, NUM_CTXT, 0, 7, masks_128_0, ctxt_dbs_128_0)
+		fmt.Println("fv: 128, #ctxt: 4", time.Since(START_TIME))
+		api.ReturnPostAPI(c, result_b, CLUSTER_IDX)
+	})
+	router.POST("/send-ctxt/face/128/13-1", api.CORSMiddleware(), func(c *gin.Context) {
+		START_TIME := time.Now()
+		var body api.RequestBody
+		err := json.NewDecoder(c.Request.Body).Decode(&body)
+		blind_auth.ReturnErr(err)
+		result_b := blind_auth.HE_Cossim(params, o, body.Ctxt, NUM_CTXT, 1, 7, masks_128_1, ctxt_dbs_128_1)
+		fmt.Println("fv: 128, #ctxt: 4", time.Since(START_TIME))
+		api.ReturnPostAPI(c, result_b, CLUSTER_IDX)
+	})
+	router.POST("/send-ctxt/face/128/13-2", api.CORSMiddleware(), func(c *gin.Context) {
+		START_TIME := time.Now()
+		var body api.RequestBody
+		err := json.NewDecoder(c.Request.Body).Decode(&body)
+		blind_auth.ReturnErr(err)
+		result_b := blind_auth.HE_Cossim(params, o, body.Ctxt, NUM_CTXT, 2, 7, masks_128_2, ctxt_dbs_128_2)
+		fmt.Println("fv: 128, #ctxt: 4", time.Since(START_TIME))
+		api.ReturnPostAPI(c, result_b, CLUSTER_IDX)
+	})
+	router.POST("/send-ctxt/face/128/13-3", api.CORSMiddleware(), func(c *gin.Context) {
+		START_TIME := time.Now()
+		var body api.RequestBody
+		err := json.NewDecoder(c.Request.Body).Decode(&body)
+		blind_auth.ReturnErr(err)
+		result_b := blind_auth.HE_Cossim(params, o, body.Ctxt, NUM_CTXT, 3, 7, masks_128_3, ctxt_dbs_128_3)
+		fmt.Println("fv: 128, #ctxt: 8", time.Since(START_TIME))
+		api.ReturnPostAPI(c, result_b, CLUSTER_IDX)
+	})
+
+	router.POST("/send-ctxt/face/128/13-4", api.CORSMiddleware(), func(c *gin.Context) {
+		START_TIME := time.Now()
+		var body api.RequestBody
+		err := json.NewDecoder(c.Request.Body).Decode(&body)
+		blind_auth.ReturnErr(err)
+		input_ct_b := body.Ctxt
+		result_b := blind_auth.HE_Cossim(params, o, input_ct_b, NUM_CTXT, 4, 7, masks_128_4, ctxt_dbs_128_4)
+		fmt.Println("fv: 128, #ctxt: 16", time.Since(START_TIME))
+		api.ReturnPostAPI(c, result_b, CLUSTER_IDX)
+	})
+
+	router.POST("/send-ctxt/face/128/13-5", api.CORSMiddleware(), func(c *gin.Context) {
+		START_TIME := time.Now()
+		var body api.RequestBody
+		err := json.NewDecoder(c.Request.Body).Decode(&body)
+		blind_auth.ReturnErr(err)
+		result_b := blind_auth.HE_Cossim(params, o, body.Ctxt, NUM_CTXT, 5, 7, masks_128_5, ctxt_dbs_128_5)
+		fmt.Println("fv: 128, #ctxt: 32", time.Since(START_TIME))
+		api.ReturnPostAPI(c, result_b, CLUSTER_IDX)
+	})
+
+	// ======================================================== //
+	// Api for feature vector size = 64
+	// ======================================================== //
+	router.POST("/send-ctxt/face/64/13-0", api.CORSMiddleware(), func(c *gin.Context) {
+		START_TIME := time.Now()
+		var body api.RequestBody
+		err := json.NewDecoder(c.Request.Body).Decode(&body)
+		blind_auth.ReturnErr(err)
+		result_b := blind_auth.HE_Cossim(params, o, body.Ctxt, NUM_CTXT, 0, 6, masks_64_0, ctxt_dbs_64_0)
+		fmt.Println("fv: 64, #ctxt: 4", time.Since(START_TIME))
+		api.ReturnPostAPI(c, result_b, CLUSTER_IDX)
+	})
+	router.POST("/send-ctxt/face/64/13-1", api.CORSMiddleware(), func(c *gin.Context) {
+		START_TIME := time.Now()
+		var body api.RequestBody
+		err := json.NewDecoder(c.Request.Body).Decode(&body)
+		blind_auth.ReturnErr(err)
+		result_b := blind_auth.HE_Cossim(params, o, body.Ctxt, NUM_CTXT, 1, 6, masks_64_1, ctxt_dbs_64_1)
+		fmt.Println("fv: 64, #ctxt: 1", time.Since(START_TIME))
+		api.ReturnPostAPI(c, result_b, CLUSTER_IDX)
+	})
+	router.POST("/send-ctxt/face/64/13-2", api.CORSMiddleware(), func(c *gin.Context) {
+		START_TIME := time.Now()
+		var body api.RequestBody
+		err := json.NewDecoder(c.Request.Body).Decode(&body)
+		blind_auth.ReturnErr(err)
+		result_b := blind_auth.HE_Cossim(params, o, body.Ctxt, NUM_CTXT, 2, 6, masks_64_2, ctxt_dbs_64_2)
+		fmt.Println("fv: 64, #ctxt: 2", time.Since(START_TIME))
+		api.ReturnPostAPI(c, result_b, CLUSTER_IDX)
+	})
+
+	router.POST("/send-ctxt/face/64/13-3", api.CORSMiddleware(), func(c *gin.Context) {
+		START_TIME := time.Now()
+		var body api.RequestBody
+		err := json.NewDecoder(c.Request.Body).Decode(&body)
+		blind_auth.ReturnErr(err)
+		result_b := blind_auth.HE_Cossim(params, o, body.Ctxt, NUM_CTXT, 3, 6, masks_64_3, ctxt_dbs_64_3)
+		fmt.Println("fv: 64, #ctxt: 8", time.Since(START_TIME))
+		api.ReturnPostAPI(c, result_b, CLUSTER_IDX)
+	})
+	router.POST("/send-ctxt/face/64/13-4", api.CORSMiddleware(), func(c *gin.Context) {
+		START_TIME := time.Now()
+		var body api.RequestBody
+		err := json.NewDecoder(c.Request.Body).Decode(&body)
+		blind_auth.ReturnErr(err)
+		result_b := blind_auth.HE_Cossim(params, o, body.Ctxt, NUM_CTXT, 4, 6, masks_64_4, ctxt_dbs_64_4)
+		fmt.Println("fv: 64, #ctxt: 16", time.Since(START_TIME))
+		api.ReturnPostAPI(c, result_b, CLUSTER_IDX)
+	})
+
+	router.Run(":18888")
+}
